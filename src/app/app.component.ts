48ba576c1879a4569ad19edac236fe4666478d13
Navbryce <irvingwashington01@gmail.com>
2017-10-08 21:52:24 -0400

    Project. Main page mostly complete.

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import * as boats from '../../Data/boats.json';
+
+const ships: Array<Ship> = (<any>boats).boats;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  title = 'Warship Website';
+  shipsList = ships;
+}
+export class Ship {
+    id: number;
+    name: string;
+    weaponSystems: Array<Weapon>;
+    thumbnail: string;
+}
+export class Weapon{
+    id: number;
+    name: string;
+    quantity: number;
+}
