bff1b4e51a4b49d16e993391018e503162f524e4
Bryce Plunkett <irvingwashington01@gmail.com>
2018-01-08 21:05:30 -0500

    Spent most of the time researching Mongoose and improving code readibility. Also learned about Promises and implemented them into the code.

--- Backend/Routes/ships.js
+++ Backend/Routes/ships.js
@@ -1,9 +1,55 @@
 const express = require('express');
 const router = express.Router();
+var mongoose = require('mongoose');
 
-/* GET api listing. */
-router.get('/', (req, res) => {
-    res.send('You have attempted to access the ships backend. Return to main page.');
+var shipSchema = mongoose.Schema({
+  scrapeURL: String,
+  configuration: String,
+  displayName: String,
+  name: String,
+  importantDates: [Object],
+  awards: [Object],
+  armament: [Object],
+  armor: [Object],
+  description: String,
+  physicalAttributes: Object,
+  pictures: [Object],
+  any: Object // Catch all. Ships are dynamic and don't always have the same attributes
 });
+var Ship = mongoose.model('ships', shipSchema);
+
+// Connect to Mongo Database 'ABoat'
+var connectedToDatabase = false;
+mongoose.connect('mongodb://localhost/ABoat', { useMongoClient: true });
+var database = mongoose.connection;
+database.on('error', console.error.bind(console, 'Error when connecting to database.'));
+database.once('open', function () {
+  connectedToDatabase = true;
+});
+
+router.get('/', function (req, res) {
+  res.send('You have attempted to access the ships backend. Return to main page.');
+});
+
+router.post('/getAllShips', function (req, res) {
+  getAllShips().then((data) => {
+    res.json(data);
+  });
+});
+
+// filter must be in the format of MongoQuerying syntax. returns promise if successful
+function getShips (filter) {
+  if (connectedToDatabase) {
+    return Ship.find(filter).exec(); // Returns ship promise
+  } else {
+    throw new Error('The server is not connected to a database');
+  }
+}
+// returns promise
+function getAllShips () {
+  var filter = {};
+  var promise = getShips(filter);
+  return promise;
+}
 
 module.exports = router;
