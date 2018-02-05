48a11f4bc553f85027037e8a10abfc614a40d40e
Bryce Plunkett <irvingwashington01@gmail.com>
2018-02-05 11:16:06 -0500

    Generated sort-by option list instead of defining option list in HTML

--- src/app/navbar/app.navbar.component.ts
+++ src/app/navbar/app.navbar.component.ts
@@ -13,6 +13,7 @@ export class NavbarComponent {
   searchEntry = "";
   sortBy = "displayName";
   sortOrder = "1";
+  sortByList = this.generateSortBy();
 
   addShipButtonClicked(): void {
     this.searchService.toggleAddShip(true);
@@ -25,8 +26,37 @@ export class NavbarComponent {
     this.searchService.changeSortOrder(parseInt(this.sortOrder));
   }
 
+  // Generate sortby options
+  generateSortBy(): Array<Option> {
+    var sortByList = [];
+    var sortBy = [
+      {value: "displayName", text: "Name"},
+      {value: "complement", text: "Complement"},
+      {value: "armament.normalGun.sizeCalculate.numberOfValues", text: "Number of Guns"},
+      {value: "armament.normalGun.sizeCalculate.average", text: "Average Size of Guns"},
+      {value: "armament.normalGun.sizeCalculate.maxValue", text: "Largest Gun"},
+      {value: "armament.normalGun.sizeCalculate.minValue", text: "Smallest Gun"},
+      {value: "armament.normalGun.sizeCalculate.sum", text: "Sum of All Guns"},
+      {value: "armament.normalGun.sizeCalculate.mode.value", text: "Mode Gun Size"},
+      {value: "physicalAttributes.speed.value", text: "Speed"},
+    ]
+    sortBy.forEach((sortByObject) => { // Puts it into a standardized form
+      var option = new Option(sortByObject.value, sortByObject.text);
+      sortByList.push(option);
+    });
+
+    return sortByList;
+  }
+
   constructor(private searchService: SearchService) { }
+}
 
+export class Option {
+  optionValue: string;
+  optionText: string;
 
-
+  constructor (value: string, text: string) {
+    this.optionValue = value;
+    this.optionText = text;
+  }
 }
