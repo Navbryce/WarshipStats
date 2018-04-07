24982770992e33a0278ecd72b01482eeb87c8047
Bryce Plunkett <irvingwashington01@gmail.com>
2018-04-07 15:20:38 -0400

    Changed the file path a little

--- Backend/Modules/ConfigModule/index.js
+++ Backend/Modules/ConfigModule/index.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
 var config = null;
+const appPath = process.env.SHIP_APP; // Path points to parent directory of ship app. Ship scraper should be set under the SHIP_APP environment variable
 
-var configContents = fs.readFileSync('/app/config.json').toString();
+var configContents = fs.readFileSync(appPath + '/config.json').toString();
 config = JSON.parse(configContents);
 
 function getConfig () {
