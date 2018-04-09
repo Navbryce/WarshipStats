9f029ec16f9b565f8c7a2b9cb27477a51de00a27
Bryce Plunkett <irvingwashington01@gmail.com>
2018-04-09 19:49:44 -0400

    Added timestamps to log. Logfile individual strings are split up by new lines so each new line has its own time stamp (doesn't work for python output for some reason)

--- Backend/Modules/LogModule/index.js
+++ Backend/Modules/LogModule/index.js
@@ -1,11 +1,8 @@
 var fs = require('fs');
 var util = require('util');
+var moment = require('moment');
 
 function LogWriter (pathInsideLogFolder, logName) { // Assumes the SHIP_APP path has been set.pathInsideLogFolder should include a back slash / at the end
-  this.log = (string) => {
-    string = util.format(string) + '\n';
-    writeStream.write(string);
-  };
   this.deleteLog = () => {
     writeStream.close();
     fs.unlink(this.logPath, (error) => {
@@ -13,7 +10,20 @@ function LogWriter (pathInsideLogFolder, logName) { // Assumes the SHIP_APP path
       console.error(error);
     });
   };
+  this.log = (string) => {
+    var stringsList = getArrayOfStrings(string); // If there are multiple lines, break them up so a timestamp can be added to each. Timestamp won't be reflective of actual time of line because we're breaking them up post fact
+    stringsList.forEach((individualString) => {
+      individualString = moment().format() + ': ' + util.format(string) + '\r\n';
+      this.rawLog(individualString);
+    });
+  };
+  this.rawLog = (string) => {
+    writeStream.write(string); // Writes string immediatly to log. Does nothing to it
+  };
 
+  function getArrayOfStrings (string) { // Will return an array of strings. A single string is broken into elements of an array if therer is a space
+    return string.split(/\r\n|\r|\n/g); // Doesn't always work for python for some reason
+  }
   // Constructor
   const appPath = process.env.SHIP_APP; // Path points to parent directory of ship app. Ship scraper should be set under the SHIP_APP environment variable
   this.logPath = appPath + '/Logs/' + pathInsideLogFolder + logName + '.log';
@@ -26,7 +36,7 @@ function LogWriter (pathInsideLogFolder, logName) { // Assumes the SHIP_APP path
   var writeStream = fs.createWriteStream(this.logPath, {flags: 'w'});
 
   if (existingLogContents != null) {
-    this.log(existingLogContents + '\n');
+    this.log(existingLogContents);
     this.log('ALL DATA BEFORE THIS POINT WAS FROM A PRE-EXISTING LOG FILE');
   }
 }
